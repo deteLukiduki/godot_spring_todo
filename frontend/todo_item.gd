1de9d6acb673998998edf47835822ce8c9d565db
deteLukiduki <41967720+deteLukiduki@users.noreply.github.com>
2022-06-25 22:38:14 +0200

    made text visible in frontend

---
+++ frontend/todo_item.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func setText(var text):
+	$TextEdit.text = text
--- frontend/todo_page.gd
+++ frontend/todo_page.gd
@@ -1,7 +1,7 @@
 extends Control
 
 var todo_path = "res://todo_item.tscn"
-
+var todo_res = load(todo_path)
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -9,10 +9,18 @@ var todo_path = "res://todo_item.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var todo_res = load(todo_path)
-	var todoItem = todo_res.instance()
-	$HBoxContainer/priority1/VBoxContainer.add_child(todoItem)
+	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
+	$HTTPRequest.request("http://localhost:8090/todo")
 
+
+func _on_request_completed(result, response_code, headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
+	if json:
+		print(json.result)
+		var todoItem = todo_res.instance()
+		todoItem.setText(json.result.text)
+		$HBoxContainer/priority1/VBoxContainer.add_child(todoItem)
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
