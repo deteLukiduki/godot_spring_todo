6213ea9854ea5c0dc84ca7eb21b19ca766184b88
deteLukiduki <41967720+deteLukiduki@users.noreply.github.com>
2022-06-25 22:44:07 +0200

    created get todos function with dummy data

--- backend/src/main/java/de/hs/vs/VsApplication.java
+++ backend/src/main/java/de/hs/vs/VsApplication.java
@@ -1,5 +1,8 @@
 package de.hs.vs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -10,6 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 @EntityScan("de.hs.*")
 @RestController
 public class VsApplication {
+
+	List<ToDoItem> todoItems = new ArrayList<>();
+
+	public VsApplication() {
+		ToDoItem item1 = new ToDoItem();
+		item1.setText("Play games");
+		item1.setPriority(1);
+		todoItems.add(item1);
+
+		ToDoItem item2 = new ToDoItem();
+		item2.setText("Study");
+		item2.setPriority(1);
+		todoItems.add(item2);
+
+		ToDoItem item3 = new ToDoItem();
+		item3.setText("Wash my car");
+		item3.setPriority(3);
+		todoItems.add(item3);
+	}
 
 	@GetMapping("/test")
 	public String testMethod() {
@@ -23,6 +45,11 @@ public class VsApplication {
 		temp.setPriority(1);
 
 		return temp;
+	}
+
+	@GetMapping("/todos")
+	public List<ToDoItem> getTodoItems() {
+		return this.todoItems;
 	}
 
 	public static void main(String[] args) {
